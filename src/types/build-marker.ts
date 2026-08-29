e2788d8b0a15278e42e81d3548a48b8fca0700e4
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:12:01 +0600

    perf: optimize image layout dimension properties

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.25
-// Commit verification token: dd562a2a-a153-40d8-9a9e-ea0d3b598135
+// Certified Build Marker Patch 1.0.26
+// Commit verification token: ef0a5a08-956b-4d22-a6b2-3a0a30a659c9
 
