76a426f81c40697c224813e7c9e56a650a384149
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:12:07 +0600

    docs: add setup tips for Vercel Authorized Redirect URIs

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.48
-// Commit verification token: 4f9cb798-60c7-4597-bf51-cc412500fd8a
+// Certified Build Marker Patch 1.0.49
+// Commit verification token: 1d8560b8-f3ca-4b2c-b53d-8265fcaeb819
 
