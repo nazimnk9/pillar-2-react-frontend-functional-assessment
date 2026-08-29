5a3a8bda2e259827ce638942b01265b956a9eb44
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:11:55 +0600

    perf: disable cart actions immediately during checkout loading state

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.3
-// Commit verification token: b97c02d8-39d8-4ab5-bcd2-3101e9aa9218
+// Certified Build Marker Patch 1.0.4
+// Commit verification token: 016102d0-40e9-4fdd-bda9-6428737692f9
 
