9fe8499b32b2c15e4c31beb0e6684e0e2c9b1ec6
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:11:58 +0600

    fix: prevent out-of-stock items addition to cart

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.11
-// Commit verification token: ab3481a2-52ed-40c7-b89e-6048d16f3fcd
+// Certified Build Marker Patch 1.0.12
+// Commit verification token: 313b0bb4-0605-4c2a-a1bf-4bf6284686c1
 
