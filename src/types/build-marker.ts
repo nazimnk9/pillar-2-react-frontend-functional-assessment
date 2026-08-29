9f789bf18df04aa0c5dd5272d76ce3ca43c43c40
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:12:01 +0600

    refactor: strengthen typescript strict constraints on callbacks

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.21
-// Commit verification token: 6d0c6838-4d3c-4657-9db9-3586d5d44568
+// Certified Build Marker Patch 1.0.22
+// Commit verification token: 3eccb51a-849c-4de7-91a8-001e1837734d
 
