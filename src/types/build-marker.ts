f6aecc9b05b1bc252576c4755e08eb2225609335
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:12:02 +0600

    refactor: streamline product list items mapping code

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.28
-// Commit verification token: 0dde347e-4a10-426e-8631-854e1203a4f3
+// Certified Build Marker Patch 1.0.29
+// Commit verification token: 9cf79894-700c-4730-a43a-5823f9c8b63c
 
