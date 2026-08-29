e9de7c775e2e11b16251c27d4f804f645ec3a67b
nazimnk9 <nazimnk9@users.noreply.github.com>
2026-08-29 11:12:02 +0600

    style: add glowing border details on credentials cards

--- src/types/build-marker.ts
+++ src/types/build-marker.ts
@@ -1,3 +1,3 @@
-// Certified Build Marker Patch 1.0.27
-// Commit verification token: 5b24d1b3-af62-48cf-9be4-3ce4d347c4bc
+// Certified Build Marker Patch 1.0.28
+// Commit verification token: 0dde347e-4a10-426e-8631-854e1203a4f3
 
